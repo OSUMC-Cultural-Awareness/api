a005fc2213039234bba29d3c86a98c117b8a3e0b
Nick Hackman <snickhackman@gmail.com>
2020-12-01 17:35:32 -0500

    Tests: feedback tests
    Added tests for feedback

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,3 +3,23 @@ def test_health(client):
     assert response.status_code == 200
 
     assert response.get_json() == {"msg": "healthy"}
+
+
+def test_feedback(client):
+    res = client.post("/api/v1/feedback", json={"feedback": "this is a test"})
+    assert res.status_code == 200
+
+
+def test_feedback_invalid_request(client):
+    res = client.post("/api/v1/feedback", json={"feedbacks": "this is a test"})
+    assert res.status_code == 400
+
+
+def test_feedback_invalid_too_short(client):
+    res = client.post("/api/v1/feedback", json={"feedback": ""})
+    assert res.status_code == 400
+
+
+def test_feedback_invalid_too_long(client):
+    res = client.post("/api/v1/feedback", json={"feedback": "hi" * 300})
+    assert res.status_code == 400
